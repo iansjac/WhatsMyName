9d4b57ad379171635b1ff095d9402cae68ec78bd
Ian Jacobs <iansjacobs@yahoo.com>
2016-01-13 11:23:10 -0500

    Initial Commit

---
+++ WhatsMyName/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WhatsMyName
+//
+//  Created by Ian Jacobs on 1/13/16.
+//  Copyright © 2016 Ian Jacobs studios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
